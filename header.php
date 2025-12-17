d44bbf2cc7b762598a5010b72a5c906e0034137b
Shawrov <intshawrov@gmail.com>
2025-12-17 19:17:08 +0600

    updated

--- header.php
+++ header.php
@@ -75,12 +75,20 @@
 
     
     <nav class="cmn-tile-nav">
-
 	<?php wp_nav_menu(array(
 	'theme_location' => 'main-menu',
 	'menu_class' => 'clearfix'
     ));
 
     ?>
-    
+		<!-- <ul class="clearfix">
+			<li class="colour-1"><a href="index.html">Home</a></li>
+			<li class="colour-2"><a href="menu.html">Menu</a></li>
+			<li class="colour-3"><a href="location.html">Location</a></li>
+			<li class="colour-4"><a href="archive.html">Blog</a></li>
+			<li class="colour-5"><a href="reservation.html">Reservation</a></li>
+			<li class="colour-6"><a href="staff.html">Our Staff</a></li>
+			<li class="colour-7"><a href="news.html">News</a></li>
+			<li class="colour-8"><a href="gallery.html">Gallery</a></li>
+		</ul> -->
     </nav>
