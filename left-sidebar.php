01df623d71c9719133fd793a7af3327c26143e26
Shawrov <intshawrov@gmail.com>
2025-12-18 16:09:27 +0600

    updated

---
+++ left-sidebar.php
@@ -0,0 +1,45 @@
+<?php get_header(); ?>
+	
+
+
+<!--////////////////////////////////////Container-->
+<section id="container" class="sub-page">
+	<div class="wrap-container zerogrid">
+		<div class="crumbs">
+			<ul>
+				<li><a href="<?php echo site_url(); ?>">Home</a></li>
+
+                        <?php while( have_posts() ) : the_post(); ?>
+                        <?php if( !is_front_page() ) : ?>
+				<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                        <?php endif; ?>
+                        <?php endwhile; ?>
+			</ul>
+		</div>
+		<div id="main-content" class="col-2-3">
+			<div class="wrap-content">
+
+
+				<?php while( have_posts() ) : the_post(); ?>
+
+				<article>
+					
+					<div class="art-content">
+						<?php the_content(); ?>
+					</div>
+					
+				</article>
+
+				<?php endwhile; ?>
+
+				
+			</div>
+		</div>
+		
+		<?php get_sidebar(); ?>
+	</div>
+</section>
+
+<!--////////////////////////////////////Footer-->
+
+<?php get_footer(); ?>
