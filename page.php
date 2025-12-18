07a1594e19a0177ed876529d093ecc1ff8b0c2ae
Shawrov <intshawrov@gmail.com>
2025-12-18 15:55:03 +0600

    updated

---
+++ page.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+	
+
+
+<!--////////////////////////////////////Container-->
+<section id="container" class="sub-page">
+	<div class="wrap-container zerogrid">
+		<div class="crumbs">
+			<ul>
+				<li><a href="index.html">Home</a></li>
+
+                        <?php while( have_posts()) : the_post(); ?>
+				<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                        <?php endwhile; ?>
+			</ul>
+		</div>
+		<div id="main-content" class="col-2-3">
+			<div class="wrap-content">
+
+
+				<?php while( have_posts() ) : the_post(); ?>
+
+				<article>
+					
+					<div class="art-content">
+						<?php the_content(); ?>
+					</div>
+					
+				</article>
+
+				<?php endwhile; ?>
+
+				
+			</div>
+		</div>
+		
+		<?php get_sidebar(); ?>
+	</div>
+</section>
+
+<!--////////////////////////////////////Footer-->
+
+<?php get_footer(); ?>
