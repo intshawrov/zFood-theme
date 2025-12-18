368c73f0695c46274ea3b865a747c224f3a9aaf9
Shawrov <intshawrov@gmail.com>
2025-12-18 15:16:28 +0600

    updated

--- index.php
+++ index.php
@@ -13,9 +13,13 @@
 		</div>
 		<div id="main-content" class="col-2-3">
 			<div class="wrap-content">
+
+
+				<?php while(have_posts(): the_post(); ) ?>
+
 				<article>
 					<div class="art-header">
-						<a href="#"><h3>Nam libero tempore, cum soluta nobis est </h3></a>
+						<a href="#"><h3><?php the_title(); ?> </h3></a>
 						<div class="info">Posted on June 22, 2010 in: <a href="#">Event</a></div>
 					</div>
 					<div class="art-content">
@@ -24,28 +28,10 @@
 					</div>
 					<a class="button button02" href="#">MORE</a>
 				</article>
-				<article>
-					<div class="art-header">
-						<a href="#"><h3>Nam libero tempore, cum soluta nobis est </h3></a>
-						<div class="info">Posted on June 22, 2010 in: <a href="#">Event</a></div>
-					</div>
-					<div class="art-content">
-						<img src="<?php echo get_template_directory_uri(); ?>/images/4.jpg" />
-						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. At vero eos et accusam et justo duo dolores et ea rebum. Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-					</div>
-					<a class="button button02" href="#">MORE</a>
-				</article>
-				<article>
-					<div class="art-header">
-						<a href="#"><h3>Nam libero tempore, cum soluta nobis est </h3></a>
-						<div class="info">Posted on June 22, 2010 in: <a href="#">Event</a></div>
-					</div>
-					<div class="art-content">
-						<img src="<?php echo get_template_directory_uri(); ?>/images/5.jpg" />
-						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. At vero eos et accusam et justo duo dolores et ea rebum. Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-					</div>
-					<a class="button button02" href="#">MORE</a>
-				</article>
+
+				<?php endwhile; ?>
+
+				
 			</div>
 		</div>
 		
