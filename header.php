adefd76ca635aec234d01b8fabc03fceb82cc3d6
Shawrov <intshawrov@gmail.com>
2025-12-24 17:19:23 +0600

    updated

--- header.php
+++ header.php
@@ -60,7 +60,7 @@
 	<!--////////////////////////////////////Header-->
 	<header>
 		<div class="zerogrid">
-			<center><div class="logo"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png"></div></center>
+			<center><?php the_custom_logo(); ?></center>
 		</div>
 	</header>
       <div class="site-title">
